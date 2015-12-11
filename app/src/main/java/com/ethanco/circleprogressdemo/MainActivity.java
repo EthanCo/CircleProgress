ec0dd090c7800b453a47094b506a18eff6199e89
z1406472408 <1406472408@qq.com>
2015-12-11 13:59:31 +0800

    添加有标题的CircleProgress的组合控件的完成

--- app/src/main/java/com/ethanco/circleprogressdemo/MainActivity.java
+++ app/src/main/java/com/ethanco/circleprogressdemo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.ethanco.circleprogressdemo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -43,5 +46,22 @@ public class MainActivity extends AppCompatActivity {
                 mTickCircleProgress4.setProgress(new Random().nextInt(100));
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.startSecondActivity:
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ circleprogresslibrary/src/main/java/com/ethanco/circleprogresslibrary/FreeCircleProgress.java
@@ -0,0 +1,8 @@
+package com.ethanco.circleprogresslibrary;
+
+/**
+ * @Description TODO
+ * Created by YOLANDA on 2015-12-11.
+ */
+public class FreeCircleProgress {
+}
